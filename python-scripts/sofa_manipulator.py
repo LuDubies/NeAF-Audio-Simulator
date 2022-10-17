57eb846f298e79c520ee26605b61a2adc785afea
Luca <luca@noelscher.com>
2022-10-17 14:52:19 +0200

    adding full sofa-manipulator script producing front filter

--- python-scripts/sofa_manipulator.py
+++ python-scripts/sofa_manipulator.py
@@ -1,113 +1,96 @@
 import sofa
-import matplotlib.pyplot as plt
-import matplotlib
-from mpl_toolkits import mplot3d
 import numpy as np
 
-
-def plot_coordinates(coords, title):
-    x0 = coords
-    n0 = coords
-    fig = plt.figure(figsize=(15, 15))
-    ax = fig.add_subplot(111, projection='3d')
-    q = ax.quiver(x0[:, 0], x0[:, 1], x0[:, 2], n0[:, 0],
-                  n0[:, 1], n0[:, 2], length=0.1)
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
-    plt.title(title)
-    return q
-
-# matplotlib.use('tkagg')
+SOURCE_SOFA = r'./hrtf/MRT01.sofa'
+TARGET_SOFA = r'./hrtf/front_filter.sofa'
 
 
-HRTF_path = r'./hrtf/MRT01.sofa'
-HRTF = sofa.Database.open(HRTF_path)
-HRTF.Metadata.dump()
+def main():
+    hrtf = sofa.Database.open(SOURCE_SOFA)
+    hrtf.Metadata.dump()
+    hrtf.Dimensions.dump()
+    hrtf.Variables.dump()
 
-HRTF.Dimensions.dump()
-HRTF.Variables.dump()
+    # create own HRTF
+    measurements = hrtf.Dimensions.M
+    data_length = hrtf.Dimensions.N
+    max_string_length = 128
+    receivers = hrtf.Dimensions.R
 
-# plot Source positions
-source_positions = HRTF.Source.Position.get_values(system="cartesian")
-plot_coordinates(source_positions, 'Source positions')
+    hrir = sofa.Database.create(TARGET_SOFA, "SimpleFreeFieldHRIR",
+                                dimensions={"M": measurements, "N": data_length, "S": max_string_length})
+    try:
+        hrir.Listener.initialize(fixed=["Position", "View", "Up"])
+        hrir.Source.initialize(variances=["Position", "View"])
+        hrir.Receiver.initialize(fixed=["Position"], count=receivers)
+        hrir.Emitter.initialize(fixed=["Position"])
+
+        hrir.Data.initialize(variances=["Delay"])
+
+        hrir.Room.Type = "shoebox"
+        hrir.Room.create_attribute("Location", "fake location")
+        hrir.Room.create_variable("Temperature", ("M",))
+        hrir.Room.Temperature.Units = "celsius"
+        hrir.Room.Temperature = 28
+        hrir.Room.create_string_array("Description", ("M", "S"))
+
+        print("Attributes and metadata")
+        hrir.Metadata.dump()
+
+        print("Dimensions")
+        hrir.Dimensions.dump()
+
+        print("Variables")
+        hrir.Variables.dump()
+
+        # TODO #
+        # 1 - filter source positions for forward cone + cleanup coordinates
+        # 2 - add source positions including "fixed" ones
+        # 3 - add IR to forward cone for one receiver
+        #
+
+        # -- 1 --
+
+        def cleaner(position):
+            # set sources directly on the x,z-plane if they are in front of the listener
+            if -0.0001 < position[1] < 0.0001 and position[0] > 0:
+                position[1] = 0
+                if -0.05 < position[2] < 0.05:
+                    position[2] = 0
+                    position[0] = 1
+
+        saved_positions = hrtf.Source.Position.get_values(system="cartesian")
+
+        for pos in saved_positions:
+            cleaner(pos)
+
+        # inspect source
+        forward_positions = [pos for pos in saved_positions if -0.0001 < pos[1] < 0.0001 and pos[0] > 0]
+        print(len(forward_positions))
+        print(forward_positions)
+
+        # -- 2 --
+        hrir.Source.Position.set_values(saved_positions, system='cartesian')
+
+        # -- 3 --
+        measurement_indices = [(idx, pos) for idx, pos in enumerate(saved_positions) if pos[0] > 0.99 and abs(pos[1]) < 0.001 and abs(pos[2]) < 0.001]
+        if len(measurement_indices) > 1:
+            raise Exception("Too many valid measurements for FRONT")
+        front_measurement = measurement_indices[0][0]
+        front_ir = hrtf.Data.IR.get_values(indices={"M": front_measurement, "R": 0, "E": 0})
+        hrir.Data.IR.set_values(front_ir, indices={"M": front_measurement, "R": 0, "E": 0})
+
+        hrir.save()
+    except Exception as e:  ## sofa raises plain Exception...
+        print(f"Unsuccessful, raised {e}")
+        hrtf.close()
+        hrir.close()
+        # raise e
+    finally:
+        hrtf.close()
+        hrir.close()
+        print("Done")
 
 
-saved_measurement = HRTF.Data.IR.get_values(indices={"M": 2000, "R": 0, "E": 0})
-saved_positions = HRTF.Source.Position.get_values(system="cartesian")
-HRTF.close()
-
-# plt.show()
-
-# create own HRTF
-
-HRIR_path = r'./hrtf/selfmade_sources.sofa'
-measurements = 2304
-data_length = 256
-max_string_length = 128
-receivers = 2
-
-HRIR = sofa.Database.create(HRIR_path, "SimpleFreeFieldHRIR",
-                            dimensions={"M": measurements, "N": data_length, "S": max_string_length})
-HRIR.Listener.initialize(fixed=["Position", "View", "Up"])
-HRIR.Source.initialize(variances=["Position", "View"])
-HRIR.Receiver.initialize(fixed=["Position"], count=receivers)
-HRIR.Emitter.initialize(fixed=["Position"])
-
-HRIR.Data.initialize(variances=["Delay"])
-
-
-HRIR.Room.Type = "shoebox"
-HRIR.Room.initialize(variances=["CornerA", "CornerB"])
-HRIR.Room.create_attribute("Location", "various recording locations")
-HRIR.Room.create_variable("Temperature", ("M",))
-HRIR.Room.Temperature.Units = "celsius"
-HRIR.Room.Temperature = 28
-HRIR.Room.create_string_array("Description", ("M", "S"))
-
-print("Attributes and metadata")
-HRIR.Metadata.dump()
-
-print("Dimensions")
-HRIR.Dimensions.dump()
-
-print("Variables")
-HRIR.Variables.dump()
-
-# enter measurement from other hrtf
-HRIR.Data.IR.set_values(saved_measurement, indices={"M": 1, "R": 0, "E": 0})
-HRIR.Data.IR.set_values(saved_measurement, indices={"M": 2, "R": 1, "E": 0})
-
-# enter source positions
-HRIR.Source.Position.set_values(saved_positions, system='cartesian')
-
-# inspect source
-print(HRIR.Listener.Position.get_values(system="cartesian"))
-print(HRIR.Listener.View.get_values())
-print(HRIR.Listener.Up.get_values())
-source_positions = HRIR.Source.Position.get_values(system="spherical")
-interesting_positions = [pos for pos in source_positions if any([(0.1 > coord > -0.1) for coord in pos[:2]])]
-print(len(interesting_positions))
-print(interesting_positions)
-
-source_positions = HRIR.Source.Position.get_values(system="cartesian")
-plot_coordinates(source_positions, 'Source positions')
-
-measurement = 1
-legend = []
-
-t = np.arange(0, HRIR.Dimensions.N)*HRIR.Data.SamplingRate.get_values(indices={"M": measurement})
-
-plt.figure(figsize=(15, 5))
-for receiver in np.arange(HRIR.Dimensions.R):
-    plt.plot(t, HRIR.Data.IR.get_values(indices={"M": measurement, "R": receiver, "E": 0}))
-    legend.append('Receiver {0}'.format(receiver))
-plt.title('HRIR at M={0} for emitter {1}'.format(measurement, 0))
-plt.legend(legend)
-plt.xlabel('$t$ in s')
-plt.ylabel(r'$h(t)$')
-plt.grid()
-
-HRIR.save()
-HRIR.close()
-
-# plt.show()
+if __name__ == "__main__":
+    main()
